86e6b04a3d400fff10f6f4a71673fa366c05079c
ashwinpaulallen <ashwin23paul@outlook.com>
2021-10-09 23:51:00 +0530

    updated to latest config

---
+++ flash_chart/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flash_chart/screens/chat_screen.dart';
+import 'package:flash_chart/screens/login_screen.dart';
+import 'package:flash_chart/screens/registration_screen.dart';
+import 'package:flash_chart/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      routes: {
+        '/welcome': (context) => WelcomeScreen(),
+        '/chat': (context) => ChatScreen(),
+        '/login': (context) => LoginScreen(),
+        '/registration': (context) => RegistrationScreen()
+      },
+      initialRoute: '/welcome',
+    );
+  }
+}
